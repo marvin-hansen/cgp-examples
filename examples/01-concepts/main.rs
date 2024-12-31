b1bd9820244a6dbc582aee4c3fe4a4675fa8126c
Marvin Hansen <marvin.hansen@gmail.com>
2024-12-31 15:55:33 +0800

    Added fist two examples based on the book
     Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/01-concepts/main.rs
+++ examples/01-concepts/main.rs
@@ -1,1 +1,102 @@
-fn main() {}
+// Context
+// https://patterns.contextgeneric.dev/context.html
+
+// In CGP, we use the term context to refer to a type that provide certain functionalities,
+//  or dependencies. The most common kind of functionality a context may provide is a method.
+
+struct MyContext {}
+
+impl MyContext {
+    fn hello(&self) {
+        println!("Hello World!");
+    }
+}
+
+// Consumer
+// https://patterns.contextgeneric.dev/consumer.html#consumer
+
+// In CGP, a consumer is a piece of code that consumes certain functionalities from a context.
+// There are several ways which a consumer may consume a functionality.
+// At its most basic, if a consumer has access to the concrete type of a context,
+// it can access any methods defined by an impl block of that context.
+
+struct Person {
+    name: String,
+}
+
+impl Person {
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+#[allow(unused)]
+fn greet(person: &Person) {
+    println!("Hello, {}!", person.name());
+}
+
+// In the above example, we have a greet function that prints a greeting to a person
+// using the method Person::name. In other words, we say that
+// the greet function is a consumer to the Person::name method.
+
+// Context-Generic Consumers
+// The greet function in our previous example can only work with the Person struct.
+// However, if we inspect the implementation of greet, we can see that it is possible to generalize
+// greet to work with any type that has a name.
+//
+// To generalize greet, we first need to define a trait that acts as an interface for getting a name:
+#[allow(unused)]
+trait HasName {
+    fn name(&self) -> &str;
+}
+
+//Provider
+// https://patterns.contextgeneric.dev/provider.html
+
+// In CGP, a provider is a piece of code that implements certain functionality for a context.
+// At its most basic, a provider is consist of an impl block for a trait.
+
+impl HasName for Person {
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+// In the above example, we implement the HasName for the Person struct.
+// The block impl HasName for Person is a provider of the HasName trait for the Person context.
+
+// For this example, the impl block is a context-specific provider for the Person context.
+// Furthermore, due to the restrictions of Rust's trait system,
+// there can be at most one provider of HasName for the Person context.
+// Another common restriction is that the provider has to be defined in the same crate as either the trait or the context.
+//
+// The asymetry between what can be done with a provider, as compared to a consumer,
+// is often a source of complexity in many Rust programs. As we will learn in later chapters,
+// one of the goals of CGP is to break this asymetry, and make it easy to implement context-generic providers.
+
+// Providers as Consumers
+// Although we have providers and consumers as distinct concepts,
+// it is common to have code that serve as both providers and consumers.
+
+trait CanGreet {
+    fn greet(&self);
+}
+
+impl CanGreet for Person {
+    fn greet(&self) {
+        println!("Hello, {}!", self.name());
+    }
+}
+
+fn main() {
+    // We can then use the hello method anywhere that we have a value of type MyContext;
+    // Notice, that have bound the method hello to the context MyContext, thus making it context-specific.
+    let my_context = MyContext {};
+    my_context.hello();
+
+    //  However, the implementation below is context-specific to the Person context, and cannot be reused for other contexts.
+    let person = Person { name: "Alice".to_owned(), };
+    person.greet();
+
+    // The next example shows how to define context-generic implementations of Greet that works with any context type
+}
---
+++ examples/02-blanket-trait-impl/main.rs
@@ -0,0 +1,104 @@
+
+// In the previous chapter, we have an implementation of CanGreet for Person that makes use of
+// HasName to retrieve the person's name to be printed. However, the implementation is context-specific
+// to the Person context, and cannot be reused for other contexts.
+
+// we want to be able to define context-generic implementations of Greet that works with any context type
+// that also implements HasName. For this, the blanket trait implementations pattern is one basic way
+// which we can use for defining context-generic implementations
+
+trait HasName {
+    fn name(&self) -> &str;
+}
+
+trait CanGreet {
+    fn greet(&self);
+}
+
+impl<Context> CanGreet for Context
+    where Context: HasName {
+    fn greet(&self) {
+        println!("Hello, {}!", self.name());
+    }
+}
+
+// The above example shows a blanket trait implementation of CanGreet for any Context type that implements HasName.
+// With that, contexts like Person do not need to explicitly implement CanGreet, if they already implement HasName:
+
+struct Person { name: String }
+
+impl HasName for Person {
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+// Extension Traits
+//
+// The use of blanket trait implementation is commonly found in many Rust libraries today.
+//  For example, Itertools provides a blanket implementation for any context that implements Iterator.
+// Another example is StreamExt, which is implemented for any context that implements Stream.
+//
+// Traits such as Itertools and StreamExt are sometimes known as extension traits.
+// This is because the purpose of the trait is to extend the behavior of existing types,
+//  without having to own the type or base traits.
+
+// Overriding Blanket Implementations
+//
+// Extension Traits containing blanket implementation are usually not meant to be implemented
+// manually by individual contexts. They are usually meant to serve as convenient methods
+// that extends the functionality of another trait.
+
+// However, Rust's trait system does not completely prevent us from overriding the blanket implementation.
+//
+// Supposed that we have a VipPerson context that we want to implement a different way of greeting the VIP person.
+// We could override the implementation as follows:
+
+struct VipPerson { name: String, /* other fields */ }
+
+impl CanGreet for VipPerson {
+    fn greet(&self) {
+        println!("A warm welcome to you, {}!", self.name);
+    }
+}
+
+// Conflicting Implementations
+// In the previous example, we are able to define a custom provider for VipPerson,
+// but with an important caveat: that VipPerson does not implement HasName.
+//
+//   If we try to define a custom provider for contexts that already implement HasName,
+// such as for Person, the compilation would fail:
+
+
+// impl CanGreet for Person {
+//     fn greet(&self) {
+//         println!("Hi, {}!", self.name());
+//     }
+// }
+// ^^^^ conflicting implementation for `Person`
+
+// The reason for the conflict is because Rust trait system requires all types
+// to have unambiguous implementation of any given trait.
+// f Rust were to allow ambiguous override of blanket implementations, such as what we tried with Person,
+// it would have resulted in inconsistencies in the compiled code,
+
+fn main() {
+
+    // we call person.greet() without having a context-specific implementation of CanGreet for Person.
+    let person = Person { name: "Alice - Context Generic".to_owned() };
+    person.greet();
+
+    // Here we have a context-specific implementation of CanGreet for VipPerson attached to the VipPerson context
+    let vip_person = VipPerson { name: "Alice - VIP".to_owned() };
+    vip_person.greet();
+}
+
+// Limitations of Blanket Implementations
+//
+// Due to potential conflicting implementations, the use of blanket implementations offer limited customizability,
+// in case if a context wants to have a different implementation.
+// Although a context many define its own context-specific provider to override the blanket provider, it would face other limitations such as not being able to implement other traits that may cause a conflict.
+//
+// In practice, we consider that blanket implementations allow for a singular context-generic provider to be defined.
+// In future chapters, we will look at how to relax the singular constraint,
+// to make it possible to allow multiple context-generic or context-specific providers to co-exist.
